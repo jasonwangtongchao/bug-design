cfb600cf4c565ff60bd6b5c2973efc57d0ea3d23
王同超 <wangtongchao@kaikeba.com>
2022-04-14 15:24:15 +0800

    feat: 暂存

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+npm run docs:build
+
+# cd 到构建输出的目录下 
+cd docs-dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署
+git push -f git@github.com:tongchaowang/bug-design.git master:gh-pages
